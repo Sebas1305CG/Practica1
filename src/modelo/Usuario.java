cc6ce351308a31136f2e0c758f7d717cdc9eef37
Sebas1305CG <renny.caicedo284@ist17dejulio.edu.ec>
2024-05-30 10:05:50 -0500

    Creación de clase usuario, atributos privados y constructores

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package vista;
+package modelo;
 
 /**
  *
  * @author Lenovo.User
  */
-public class Main {
+public class Usuario {
     
 }
---
+++ src/vista/MainEstructurado.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author Lenovo.User
+ */
+public class MainEstructurado {
+    
+    public static void main(String[] args) {
+        //LISTAS
+        String[]listaDatos=new String[5];
+    //VARIABLES
+        System.out.println("DATOS PERSONALES");
+String nombre="Sebas";
+String apellido="Caicedo";
+char inicial='S';
+String direccion="Ibarra";
+String correoElectronico="renny.caicedo284@ist17dejulio.edu.ec";
+      listaDatos[0]=nombre;
+      listaDatos[1]=apellido;
+      listaDatos[2]=direccion;
+      listaDatos[3]=correoElectronico;
+        System.out.println("1)"+listaDatos[0]);
+         System.out.println("2)"+listaDatos[1]);
+          System.out.println("3)"+listaDatos[2]);
+           System.out.println("4)"+listaDatos[3]);
+            System.out.println("1)"+listaDatos[4]);
+System.out.println("DATOS PERSONALES");
+String nombres1="Sebas";
+String apellido1="Caicedo";
+char inicial1='S';
+String direccion2="Quito";
+String correoElectronico2="renny.caicedo284@ist17dejulio.edu.ec";
+    }
+}
